428da78b3a44de6dbaf1474051e991dc94143835
KoblizekXD <janprokupek@centrum.cz>
2023-07-30 18:03:03 +0200

    Initial commit

---
+++ src/main/java/io/github/koblizekxd/packetsplus/Main.java
@@ -0,0 +1,13 @@
+package io.github.koblizekxd.packetsplus;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Main implements ModInitializer {
+    /**
+     * Runs the mod initializer.
+     */
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/io/github/koblizekxd/packetsplus/client/MainClient.java
@@ -0,0 +1,15 @@
+package io.github.koblizekxd.packetsplus.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.minecraft.util.profiling.jfr.event.PacketEvent;
+import net.minecraft.util.profiling.jfr.event.PacketReceivedEvent;
+
+public class MainClient implements ClientModInitializer {
+    /**
+     * Runs the mod initializer on the client environment.
+     */
+    @Override
+    public void onInitializeClient() {
+        
+    }
+}
