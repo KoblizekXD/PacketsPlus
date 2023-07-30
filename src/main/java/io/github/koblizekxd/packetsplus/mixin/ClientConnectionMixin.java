b7abf26bda797c10a8ce2e103ecdaa4818e4820c
KoblizekXD <janprokupek@centrum.cz>
2023-07-30 18:45:16 +0200

    feat: set handlePacket to correct method and added new mixin before sending the actual packet out

--- src/main/java/io/github/koblizekxd/packetsplus/mixin/ClientConnectionMixin.java
+++ src/main/java/io/github/koblizekxd/packetsplus/mixin/ClientConnectionMixin.java
@@ -2,6 +2,8 @@ package io.github.koblizekxd.packetsplus.mixin;
 
 import io.netty.channel.ChannelHandlerContext;
 import net.minecraft.network.ClientConnection;
+import net.minecraft.network.PacketCallbacks;
+import net.minecraft.network.listener.PacketListener;
 import net.minecraft.network.packet.Packet;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -10,12 +12,16 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ClientConnection.class)
 public class ClientConnectionMixin {
-    @Inject(method = "channelRead0(Lio/netty/channel/ChannelHandlerContext;Lnet/minecraft/network/packet/Packet;)V", at = @At("HEAD"))
-    public void channelRead0pre(ChannelHandlerContext channelHandlerContext, Packet<?> packet, CallbackInfo ci) {
+    @Inject(method = "handlePacket", at = @At("HEAD"))
+    private static <T extends PacketListener> void handlePacketPre(Packet<T> packet, PacketListener listener, CallbackInfo ci) {
 
     }
-    @Inject(method = "channelRead0(Lio/netty/channel/ChannelHandlerContext;Lnet/minecraft/network/packet/Packet;)V", at = @At("RETURN"))
-    public void channelRead0post(ChannelHandlerContext channelHandlerContext, Packet<?> packet, CallbackInfo ci) {
+    @Inject(method = "handlePacket", at = @At("RETURN"))
+    private static <T extends PacketListener> void handlePacketPost(Packet<T> packet, PacketListener listener, CallbackInfo ci) {
+
+    }
+    @Inject(method = "send(Lnet/minecraft/network/packet/Packet;Lnet/minecraft/network/PacketCallbacks;)V", at = @At("HEAD"))
+    public void send(Packet<?> packet, PacketCallbacks callbacks, CallbackInfo ci) {
 
     }
 }
