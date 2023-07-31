03dc806bc0202addf012d6382ad329def52de586
KoblizekXD <janprokupek@centrum.cz>
2023-07-31 09:02:27 +0200

    docs: Javadocs for PacketInEventCallback.java

--- src/main/java/io/github/koblizekxd/packetsplus/events/PacketInEventCallback.java
+++ src/main/java/io/github/koblizekxd/packetsplus/events/PacketInEventCallback.java
@@ -5,8 +5,22 @@ import net.fabricmc.fabric.api.event.EventFactory;
 import net.minecraft.network.packet.Packet;
 import net.minecraft.util.ActionResult;
 
+/**
+ * Base event class for incoming packets.
+ * This event will be invoked <strong>only</strong> when client receives packet.
+ * To listen for this callback see <a href="https://fabricmc.net/wiki/tutorial:callbacks">this</a> tutorial on
+ * Fabric Wiki
+ * @see PacketInEventCallback#EVENT_PRE
+ * @see PacketInEventCallback#EVENT_POST
+ */
 public interface PacketInEventCallback {
+    /**
+     * Invokes when packet is received, but was not yet processed by Minecraft
+     */
     Event<PacketInEventCallback> EVENT_PRE = createBackend();
+    /**
+     * Invokes when packet is received and Minecraft has processed it
+     */
     Event<PacketInEventCallback> EVENT_POST = createBackend();
     static Event<PacketInEventCallback> createBackend() {
         return EventFactory.createArrayBacked(
